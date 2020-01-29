ef1bc8f5bf8d30835e714f5ea1c3822f2309b42c
arllainm <arllain@gmail.com>
2020-01-28 22:56:04 -0300

    Endpoint to create a new project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.json());
+
+const projects = [];
+
+// Creates a new project
+app.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+
+    const project = {
+        id,
+        title,
+        task: [],
+    };
+
+    projects.push(project);
+    return res.json(projects);
+});
+
 app.listen(port, () => console.log(`App is listening on port ${port}`));
